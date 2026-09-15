d38b4ed888cdfac5f245b054b1f40f157b9c4c37
PREMKUMAR <167052615+premkumar29820@users.noreply.github.com>
2026-09-15 21:32:13 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "https://bulkmailer-application.onrender.com/api";
+const BASE_URL = "https://bulkmailer-application.onrender.com";
 
 export function login(email, password) {
   return axios.post(`${BASE_URL}/auth/login`, { email, password });
